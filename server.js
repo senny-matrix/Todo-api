3ea550ed08a6167275d696f499ca9aa1b3e6518a
Rogers Aaron <arumeru@gmail.com>
2016-01-28 17:21:24 +0300

    Add PUT /todos/:id

--- server.js
+++ server.js
@@ -61,6 +61,42 @@ app.delete('/todos/:id', function(req, res){
     }
 });
 
+// PUT /todos/:id 
+app.put('/todos/:id', function (req, res){
+    
+    var todoID = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id:todoID});
+    
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttributes = {};
+    
+    if(!matchedTodo){
+        return res.status(404).send();
+    }
+    if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+        validAttributes.completed = body.completed;
+    }else if(body.hasOwnProperty('completed')){
+        // Bad
+        return res.status(400).send();
+    }else{
+        // Never provided attribute, no problem here
+    }
+    
+    if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+        validAttributes.description = body.description;
+    }else if(body.hasOwnProperty('description')){
+        res.status(400).send();
+    }
+    //body.hasOwnProperty('completed');
+    
+    // HERE
+    // Redundunt as extend will update matchTodo anyways
+    //matchedTodo = _.extend(matchedTodo, validAttributes);
+    // Just use extend
+    _.extend(matchedTodo, validAttributes);
+    res.json(matchedTodo);
+});
+
 app.listen(PORT, function(){
     console.log('Express listening on port ' + PORT + '!');
 })
