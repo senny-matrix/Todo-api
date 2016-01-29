fd355cd6d86dceaf047378939f59dff23fca3c3c
Rogers Aaron <arumeru@gmail.com>
2016-01-29 15:21:54 +0300

    Added variable-playground area

---
+++ variable-playground.js
@@ -0,0 +1,27 @@
+//var person = {
+//    name: 'Senny',
+//    age : 40
+//}
+//
+//function updatePerson(obj){
+////    obj = {
+////        name: 'Senny',
+////        age : 39
+////    }
+//    obj.age = 39;
+//}
+//
+//updatePerson(person);
+//
+//console.log(person);
+
+// Array Example
+var grades = [13, 15];
+
+function addGrades(grades){
+    grades.push(20);
+    debugger;
+}
+
+addGrades(grades);
+console.log(grades);
