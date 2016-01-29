c1146e269abdb62519783a0d64cf33f8cba1ebe5
Rogers Aaron <arumeru@gmail.com>
2016-01-29 16:39:22 +0300

    Final static todos

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', function(req, res){
     res.send('ToDo API Root');
 });
 
-// GET /todos
+// GET /todos?completed=true&q=work
 app.get('/todos', function(req, res){
     var queryParams = req.query;
     var filteredTodos = todos;
@@ -29,6 +29,11 @@ app.get('/todos', function(req, res){
         filteredTodos = _.where(filteredTodos, {completed:false});
     }
     
+    if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0){
+        filteredTodos = _.filter(filteredTodos, function(todo){
+            return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+        });
+    }
     res.json(filteredTodos); 
 });
 
