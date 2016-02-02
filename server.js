102063b057be72a37021b3fd40a5239e93cc6361
Rogers Aaron <arumeru@gmail.com>
2016-02-02 14:22:20 +0300

    Update PUT /todos/:id to sequelize

--- server.js
+++ server.js
@@ -109,42 +109,32 @@ app.delete('/todos/:id', function (req, res) {
 
 // PUT /todos/:id 
 app.put('/todos/:id', function (req, res) {
-
     var todoID = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {
-        id: todoID
-    });
-
     var body = _.pick(req.body, 'description', 'completed');
-    var validAttributes = {};
+    var attributes = {};
 
-    if (!matchedTodo) {
-        return res.status(404).send();
-    }
-    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-        validAttributes.completed = body.completed;
-    } else if (body.hasOwnProperty('completed')) {
-        // Bad
-        return res.status(400).send();
-    } else {
-        // Never provided attribute, no problem here
+    if (body.hasOwnProperty('completed')) {
+        attributes.completed = body.completed;
     }
 
-    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-        validAttributes.description = body.description;
-    } else if (body.hasOwnProperty('description')) {
-        res.status(400).send();
+    if (body.hasOwnProperty('description')) {
+        attributes.description = body.description;
     }
-    //body.hasOwnProperty('completed');
 
-    // HERE
-    // Redundunt as extend will update matchTodo anyways
-    //matchedTodo = _.extend(matchedTodo, validAttributes);
-    // Just use extend
-    _.extend(matchedTodo, validAttributes);
-    res.json(matchedTodo);
+    db.todo.findById(todoID).then(function (todo) {
+        if (todo) {
+            todo.update(attributes).then(function (todo) {
+                res.json(todo.toJSON());
+            }, function (e) {
+                res.status(400).json(e);
+            });
+        } else {
+            res.status(404).send();
+        }
+    }, function () {
+        res.status(500).send();
+    });
 });
-
 db.sequelize.sync().then((function () {
     app.listen(PORT, function () {
         console.log('Express listening on port ' + PORT + '!');
